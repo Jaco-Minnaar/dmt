be253854de75e859ea431233fe6cdc8ea37f2770
Jaco Minnaar <jminnaar@bscglobal.com>
2024-12-06 17:57:59 +0200

    fixup migrate proc macro

--- crates/libdmt-macros/src/lib.rs
+++ crates/libdmt-macros/src/lib.rs
@@ -1,5 +1,7 @@
-use std::os::unix::ffi::OsStrExt;
-use std::{env, fs};
+use std::fs::File;
+use std::io::Read;
+use std::path::{Path, PathBuf};
+use std::{env, io};
 
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
@@ -11,44 +13,54 @@ use syn::Token;
 
 #[proc_macro]
 pub fn migrate(input: TokenStream) -> TokenStream {
-    match migrate_inner(input.into()) {
-        Ok(stream) => stream.into(),
-        Err(e) => e.to_compile_error().into(),
-    }
-}
-
-fn migrate_inner(input: TokenStream2) -> syn::Result<TokenStream2> {
     let args = <Punctuated<syn::LitStr, Token![,]>>::parse_terminated
-        .parse(input.into())
+        .parse(input)
         .expect("expected macro to be called with a comma-separated list of string literals");
 
     if !args.is_empty() {
-        return Err(syn::Error::new(
-            args.span(),
-            "migrate! does not take any arguments",
-        ));
+        return syn::Error::new(args.span(), "migrate! does not take any arguments")
+            .to_compile_error()
+            .into();
     }
 
-    let Ok(dir_path) = env::current_dir() else {
-        return Ok(quote! {
+    migrate_inner().into()
+}
+
+fn migrate_inner() -> TokenStream2 {
+    let Ok(dir_path) = env::var("CARGO_MANIFEST_DIR") else {
+        return quote! {
             compile_error!("Could not get path to current dir to find dmt config file.");
-        });
+        };
     };
 
-    let Ok(dir) = fs::read_dir(&dir_path) else {
-        let msg = format!(
-            "Could not open directory {}",
-            String::from_utf8_lossy(dir_path.as_os_str().as_bytes())
-        );
-        return Ok(quote! {
-            compile_error!(#msg);
-        });
+    let mut path = PathBuf::from(dir_path);
+    path.push("dmt.config.toml");
+
+    let Ok(contents) = get_file_contents(path) else {
+        return quote! {
+            compile_error!("Could not read dmt config file.");
+        };
     };
 
-    Ok(quote! {
-        let __dmt_config_contents = include_str!("dmt.config.toml");
-        let __dmt_config = <::libdmt::DmtConfig as ::std::str::FromStr>::from_str(__dmt_config_contents);
-        let mut __dmt_db = <::libdmt::MigrationDatabase as ::std::convert::TryFrom<::libdmt::DmtConfig>>::try_from(__dmt_config).unwrap();
-        ::libdmt::run_migrations(__dmt_db, &__dmt_config.migration.migration_path).unwrap();
-    })
+    quote! {
+        {
+            use ::libdmt::{DmtConfig as __DmtConfig, MigrationDatabase as __MigrationDb, run_migrations as __run_dmt};
+            use ::std::{str::FromStr as __FromStr, convert::TryFrom as __TryFrom};
+
+            let __dmt_config_contents = #contents;
+            let __dmt_config = <__DmtConfig as __FromStr>::from_str(__dmt_config_contents).unwrap();
+            let mut __dmt_db = <__MigrationDb as __TryFrom<&__DmtConfig>>::try_from(&__dmt_config).unwrap();
+            __run_dmt(&mut __dmt_db, &__dmt_config.migration.migration_path).unwrap();
+        }
+    }
+}
+
+fn get_file_contents(path: impl AsRef<Path>) -> Result<String, io::Error> {
+    let mut config_contents = String::new();
+
+    File::open(path)?.read_to_string(&mut config_contents)?;
+
+    Ok(config_contents)
+}
+
 }
